756610792532b9b4c0a1613cad4cb9b25baa2b12
Rakesh <gururakesh85@gmail.com>
2018-09-10 23:15:46 -0500

    Arrays examples
    arrays examples

---
+++ Arrays.js
@@ -0,0 +1,16 @@
+var car1 = [];
+var car2 = new Array();
+var car3 = new Array(10);
+var car4 = new Array(4, 34, 6, 8, 525, 8693, 281, 88, 28, 95, 346);
+
+
+
+
+console.log("Creating the arrray var car1 = []");
+console.log(car1);
+console.log("Creating the arrray var car2 = new Array()");
+console.log(car2);
+console.log("Creating the arrray var car3 = new Array(10)");
+console.log(car3);
+console.log("Creating the arrray var car4 = new Array(4, 34, 6, 8, 525, 8693, 281, 88, 28, 95, 346)");
+console.log(car4);
---
+++ Basics.js
@@ -0,0 +1,43 @@
+console.log("************    Javascript types     ***************");
+
+console.log("there are 4 basic types");
+
+console.log("1.  Number");
+console.log("2.  Boolean");
+console.log("3.  String");
+console.log("4.  Array");
+
+console.log("there are complex data types");
+console.log("Those are  JSON objects and Functions");
+
+console.log("null and undefined are special kind of objects");
+
+
+
+console.log("                 typeof operator              ");
+console.log("typeof 10 "+typeof 10);
+console.log("typeof 'Rakesh' " +typeof 'Rakesh');
+console.log("typeof null " +typeof null );
+console.log("typeof undefined "+typeof undefined);
+console.log("typeof function(){var x=20;}"+typeof function(){var x=20;});
+console.log("Any operation on null and undefined will give you an error");
+console.log("var x;");
+var x;
+console.log("x.length ");
+//x.length;
+
+console.log("x=null;");
+x=null;
+console.log("x.lenght");
+//x.length;
+
+console.log("Even after declaring the variable one more time,the value remins same");
+
+console.log("var y=10;");
+var y=10;
+console.log("vy+=5;");
+y+=5;
+console.log("var y;");
+var y;
+console.log("y"+y)
+
